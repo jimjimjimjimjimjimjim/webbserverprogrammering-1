797f6591476293662f185756fbb4fd9f214a3ee6
jimmy <jimmyj@gymnasium.ax>
2019-01-24 09:42:27 +0200

    sista i kap. 4 kvar

--- templates/list-blog-posts.php
+++ templates/list-blog-posts.php
@@ -2,8 +2,15 @@
 /**
  * Mall för listning av blogginlägg
 	
-	Fortsätt på sidan 60
 */
 ?>
-<!DOCTYPE>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+	<meta charset="utf-8" />
+	<title>De senaste blooginläggen - Läxhjälpen</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<link href="https://">
+</head>
+</html>
 
